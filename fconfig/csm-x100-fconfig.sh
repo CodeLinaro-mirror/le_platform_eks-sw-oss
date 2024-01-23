7e475c91abc475f5528f3c40c4dbfa76abb6435a
VijayaRamarao Mellempudi <quic_vmellemp@quicinc.com>
2024-01-23 12:57:09 -0800

    Dynamic Configuration files
    Change-Id: I04a5bd15544e1da7830ac50c42bf3bc4c84d264a

---
+++ fconfig/csm-x100-fconfig.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#******************************************************************************
+# Copyright (c) 2023 Qualcomm Technologies, Inc.
+# All Rights Reserved.
+# Confidential and Proprietary - Qualcomm Technologies, Inc.
+#******************************************************************************/
+
+mkdir -p /var/tmp/lassenconfig
+bash -c "echo -n /var/lib/firmware > /sys/module/firmware_class/parameters/path"
+/usr/bin/cp -f /etc/DuMgrConfig.txt /var/lib/firmware/qcom/lassen/flatimg/config/.
+/usr/bin/cp -f /etc/DuMgrConfig.txt /var/tmp/lassenconfig/.
+
+/usr/bin/cp -f /etc/splane_cfg.conf /var/lib/firmware/qcom/lassen/flatimg/config/.
+/usr/bin/cp -f /etc/splane_cfg.conf /var/tmp/lassenconfig/.
+
+/usr/bin/cp -f /etc/vf-count.conf /var/tmp/.
+bash -c "echo -n 1 > /var/tmp/fconfig-state.conf"
+while true; do
+   echo "Sleeping 5: $(date)"
+   sleep 5;
+done;
