c4df481bbba030e186df52f4968cc0c97a183920
VijayaRamarao Mellempudi <quic_vmellemp@quicinc.com>
2024-01-31 21:14:20 -0800

    Updating the config file permissions
    Change-Id: I7f3d4186909760319fd6d23198f5a5b34f41df63

--- fconfig/csm-x100-fconfig.sh
+++ fconfig/csm-x100-fconfig.sh
@@ -7,15 +7,17 @@
 
 mkdir -p /var/tmp/lassenconfig
 echo $DYNAMICCONFIG_PODNAME | rev | cut -d- -f2- | rev > /var/tmp/lassenconfig/fconfig-version.log
-bash -c "echo -n /var/lib/firmware > /sys/module/firmware_class/parameters/path"
-/usr/bin/cp -f /etc/DuMgrConfig.txt /var/lib/firmware/qcom/lassen/flatimg/config/.
-/usr/bin/cp -f /etc/DuMgrConfig.txt /var/tmp/lassenconfig/.
 
-/usr/bin/cp -f /etc/splane_cfg.conf /var/lib/firmware/qcom/lassen/flatimg/config/.
-/usr/bin/cp -f /etc/splane_cfg.conf /var/tmp/lassenconfig/.
+rsync -a /etc/DuMgrConfig.txt /var/lib/firmware/qcom/lassen/flatimg/config/.
+rsync -a /etc/DuMgrConfig.txt /var/tmp/lassenconfig/.
 
-/usr/bin/cp -f /etc/vf-count.conf /var/tmp/.
+rsync -a /etc/splane_cfg.conf /var/lib/firmware/qcom/lassen/flatimg/config/.
+rsync -a /etc/splane_cfg.conf /var/tmp/lassenconfig/.
+
+rsync -a /etc/vf-count.conf /var/tmp/.
+chmod -R 777 /var/lib/firmware/qcom/lassen/flatimg/config
 bash -c "echo -n 1 > /var/tmp/fconfig-state.conf"
+
 while true; do
    echo "Sleeping 5: $(date)"
    sleep 5;
