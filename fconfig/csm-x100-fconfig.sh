b9e0ae496d76da89df93a839d2401b4574612a0e
VijayaRamarao Mellempudi <quic_vmellemp@quicinc.com>
2024-02-21 11:08:50 -0800

    Remove the config files from fconfig
    Change-Id: I1ce2faa10b256f00567a63bb766d3fd933e0d7c9

--- fconfig/csm-x100-fconfig.sh
+++ fconfig/csm-x100-fconfig.sh
@@ -8,11 +8,9 @@
 mkdir -p /var/tmp/lassenconfig
 echo $DYNAMICCONFIG_PODNAME | rev | cut -d- -f2- | rev > /var/tmp/lassenconfig/fconfig-version.log
 
-rsync -a /etc/DuMgrConfig.txt /var/lib/firmware/qcom/lassen/flatimg/config/.
-rsync -a /etc/DuMgrConfig.txt /var/tmp/lassenconfig/.
+rsync -a /etc/config/* /var/lib/firmware/qcom/lassen/flatimg/config/.
+rsync -a /etc/config/* /var/tmp/lassenconfig/.
 
-rsync -a /etc/splane_cfg.conf /var/lib/firmware/qcom/lassen/flatimg/config/.
-rsync -a /etc/splane_cfg.conf /var/tmp/lassenconfig/.
 
 rsync -a /etc/vf-count.conf /var/tmp/.
 chmod -R 777 /var/lib/firmware/qcom/lassen/flatimg/config
