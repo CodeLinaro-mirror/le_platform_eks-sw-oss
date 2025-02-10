17b52ed2e4ff5b679bb040d7e8e1b988888b3890
Kumar Harsh <quic_harkumar@quicinc.com>
2025-02-10 14:41:16 +0530

    Cleanup health check node annotation on reboot
    HealthCheckTime annotation is removed for all reboot scenarios
    irrespective of whether it is first reboot or node is repeatedly
    rebooting
    
    Change-Id: I7ddc51f514ff3c13dfea6919594bbff1aa458bce

--- x100-operator/deploy/controllers/x100_managementpolicy_controller.go
+++ x100-operator/deploy/controllers/x100_managementpolicy_controller.go
@@ -387,6 +387,17 @@ func (r *X100ManagementPolicyReconciler) Reconcile(ctx context.Context, req ctrl
 							return reconcile.Result{}, nil
 						}
 
+						// Delete boot health timer related annotation from the node
+						annotations := node.GetAnnotations()
+						annotations = resetBootHealthTimer(annotations)
+						node.SetAnnotations(annotations)
+						err := x100Ctrl.setX100NodeAnnotations(&node, annotations, x100HealthCheckStartTime, LabelUpdateDeletionType)
+						if err != nil {
+							log.Log.Info(fmt.Sprintf("[Reboot] Failed to delete x100HealthCheckStartTime annotation from node %s",
+								node.GetName()))
+							return reconcile.Result{}, err
+						}
+
 						if !hasX100ComingUpAfterRebootLabel(labels) {
 							log.Log.Info(fmt.Sprintf("Reconciler loop hit on reboot detection, setting reboot label"))
 							// We entered reconcile due to node entering reboot path
